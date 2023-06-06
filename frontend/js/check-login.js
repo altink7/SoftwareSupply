68197fb1786d7688f53f8f61b13df14b1773a1f3
realhoffmann <julianhoffmann21@gmail.com>
2023-06-06 19:14:41 +0200

    login working

--- frontend/js/check-login.js
+++ frontend/js/check-login.js
@@ -1,38 +1,34 @@
 $(document).ready(function () {
-
-    function checkLogin() {
+    $('form').submit(function (event) {
+        event.preventDefault();
 
         var username = $('#username').val();
         var password = $('#password').val();
 
+        var data = {
+            username: username,
+            password: password
+        };
+
         $.ajax({
             type: 'POST',
-            url: 'http://localhost/SoftwareSupply/backend/api/api.php',
-            data: JSON.stringify({ username: username, password: password }),
+            url: 'http://localhost/SoftwareSupply/backend/api/api.php', // Replace with the actual URL of your login endpoint
+            data: JSON.stringify(data),
             contentType: 'application/json',
             success: function (response) {
                 if (response.status === 'success') {
-                    console.log(response + response.status);
-                    alert('Successfully logged in!');
-                    setTimeout(function () {
-                        window.location.href = "userProfil.html";
-                    }, 1000);
+                    // Login successful, redirect to a desired page
+                    window.location.href = 'userProfil.html';
                 } else {
-                    alert('Login failed. Please check your username and password.');
+                    // Invalid credentials, display an error message
+                    alert('Invalid username or password. Please try again.');
                 }
             },
             error: function (xhr, status, error) {
-                console.error(error);
-                alert('An error occurred during login. Please try again later.');
+                // Handle error case
+                alert('An error occurred while processing your request. Please try again later.');
+                console.log('AJAX error:', error);
             }
         });
-    }
-
-$('form').submit(function(event) {
-    event.preventDefault();
-
-    checkLogin();
     });
 });
-
-  
