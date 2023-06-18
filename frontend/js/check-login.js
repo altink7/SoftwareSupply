020d55f2450a3cd462e7ceb037ca961c4b124386
realhoffmann <julianhoffmann21@gmail.com>
2023-06-18 12:41:56 +0200

    login merken added

--- frontend/js/check-login.js
+++ frontend/js/check-login.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    // Check if the user is already logged in and update the navbar accordingly
+    checkLoginStatus();
+
     $('form').submit(function (event) {
         event.preventDefault();
 
@@ -13,12 +16,13 @@ $(document).ready(function () {
 
         $.ajax({
             type: 'POST',
-            url: 'http://localhost/SoftwareSupply/backend/api/api.php', 
+            url: 'http://localhost/SoftwareSupply/backend/api/api.php',
             data: JSON.stringify(data),
             contentType: 'application/json',
             success: function (response) {
                 if (response.status === 'success') {
-                    // Login successful, redirect to a desired page
+                    // Login successful, update the navbar and redirect to the profile page
+                    updateNavbar(true);
                     window.location.href = 'userProfil.html';
                 }
             },
@@ -27,4 +31,38 @@ $(document).ready(function () {
             }
         });
     });
+
+    // Function to check if the user is logged in and update the navbar accordingly
+    function checkLoginStatus() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost/SoftwareSupply/backend/api/api.php?type=login_status',
+            success: function (response) {
+                if (response.logged_in === true) {
+                    updateNavbar(true);
+                } else {
+                    updateNavbar(false);
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log('Error checking login status');
+            }
+        });
+    }
+
+    // Function to update the navbar based on the login status
+    function updateNavbar(loggedIn) {
+        var profileLink = $('#profile-link');
+        var loginLink = $('#login-link');
+
+        if (loggedIn) {
+            profileLink.text('Profil');
+            loginLink.hide();
+            profileLink.show();
+        } else {
+            profileLink.text('Login');
+            profileLink.hide();
+            loginLink.show();
+        }
+    }
 });
