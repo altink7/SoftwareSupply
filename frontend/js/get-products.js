88be1787364220453207f5857b0556659f089667
realhoffmann <julianhoffmann21@gmail.com>
2023-06-20 21:29:47 +0200

    drag&drop products to cart added

--- frontend/js/get-products.js
+++ frontend/js/get-products.js
@@ -49,14 +49,15 @@ $(document).ready(function () {
           response.forEach(function (product, index) {
             var imagePath = 'http://localhost/SoftwareSupply/backend/productpictures/' + product.image_url + '.png';
             var cardCol = $('<div class="col-md-3 d-flex"></div>');
-            var card = $('<div class="card mb-4"></div>');
+            var card = $('<div class="card mb-4 draggable"></div>');
             var cardImage = $('<img class="card-img-top small-icon" src="' + imagePath + '" alt="software">');
             var cardBody = $('<div class="card-body"></div>');
             var cardTitle = $('<h5 class="card-title">' + product.title + '</h5>');
             var cardText = $('<p class="card-text description">' + product.description + '</p>');
             var cardFooter = $('<div class="card-footer"></div>');
             var priceReview = $('<p class="price-review">Price: €' + product.price + ' | ' + product.review + '/5 &star;</p>');
-            var cardButton = $('<button class="btn btn-primary card-button"><i class="fas fa-shopping-cart"></i></button>');
+            var cardButton = $('<button class="btn btn-primary card-button add-to-cart-button" data-product-id="' + product.id + '"><i class="fas fa-shopping-cart"></i></button>');
+
 
             cardBody.append(cardTitle);
             cardBody.append(cardText);
@@ -87,7 +88,9 @@ $(document).ready(function () {
               cardText.text(truncatedDescription);
             }
 
+
             cardBody.addClass('d-flex flex-column justify-content-between');
+
 
             if (index % 4 === 0) {
               var row = $('<div class="row"></div>');
@@ -100,6 +103,20 @@ $(document).ready(function () {
               var productId = product.id;
               cart.addToCart(productId);
             });
+
+            card.draggable({ // Make the card draggable
+              revert: 'invalid',
+              helper: 'clone',
+              start: function (event, ui) {
+                ui.helper.css('width', card.width());
+                ui.helper.data('product-id', product.id); // Set the product ID as data attribute
+              }
+            });
+
+            card.append(cardImage);
+            card.append(cardBody);
+            card.append(cardFooter);
+            cardCol.append(card);
           });
         } else {
           console.error('Error: No data found.');
@@ -112,4 +129,13 @@ $(document).ready(function () {
   }
 
   fetchProducts(apiUrl);
+
+  // Make shopping cart icon droppable
+  $('.shopping-cart-button').droppable({
+    accept: '.draggable',
+    drop: function (event, ui) {
+      var productId = ui.helper.data('product-id');
+      cart.addToCart(productId);
+    }
+  });
 });
