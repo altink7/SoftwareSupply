90ff054a2aadf421d92a313f28876344558ab64c
realhoffmann <julianhoffmann21@gmail.com>
2023-06-20 21:18:38 +0200

    fixed some language issues and design of the shop

--- frontend/js/get-products.js
+++ frontend/js/get-products.js
@@ -1,108 +1,115 @@
-$(document).ready(function() {
-    var categoryDropdown = $('.category-dropdown');
-    var apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=products';
-  
-    categoryDropdown.on('change', function() {
-      var selectedCategory = $(this).val();
-      var searchQuery = $('.search-input').val();
-  
-      if (selectedCategory !== 'none') {
-        apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsByCategory&kategorie=' + selectedCategory;
-      } else {
-        apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=products';
-      }
-  
-      if (searchQuery !== '') {
-        apiUrl += '&search=' + searchQuery;
-      }
-  
-      fetchProducts(apiUrl);
-    });
-  
-    $('.search-input').on('input', function() {
-      var selectedCategory = $('.category-dropdown').val();
-      var searchQuery = $(this).val();
+$(document).ready(function () {
+  var categoryDropdown = $('.category-dropdown');
+  var apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=products';
+
+  categoryDropdown.on('change', function () {
+    var selectedCategory = $(this).val();
+    var searchQuery = $('.search-input').val();
+
+    if (selectedCategory !== 'none') {
+      apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsByCategory&kategorie=' + selectedCategory;
+    } else {
       apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=products';
-  
-      if (selectedCategory !== 'none') {
-        apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsByCategory&kategorie=' + selectedCategory;
-      }
-  
-      if (searchQuery !== '') {
-        apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsBySearch&search=' + searchQuery;
-      }
-  
-      fetchProducts(apiUrl);
-    });
-  
-    function fetchProducts(apiUrl) {
-      $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-        url: apiUrl,
-        success: function(response) {
-          if (response.length > 0) {
-            var productList = $('#productList');
-            productList.empty();
-  
-            response.forEach(function(product, index) {
-              var imagePath = 'http://localhost/SoftwareSupply/backend/productpictures/' + product.image_url + '.png';
-              var cardCol = $('<div class="col-md-3 d-flex"></div>');
-              var card = $('<div class="card mb-4"></div>');
-              var cardImage = $('<img class="card-img-top small-icon" src="' + imagePath + '" alt="software">');
-              var cardBody = $('<div class="card-body"></div>');
-              var cardTitle = $('<h5 class="card-title">' + product.title + '</h5>');
-              var cardText = $('<p class="card-text">' + product.description + '</p>');
-              var cardFooter = $('<div class="card-footer"></div>');
-              var priceReview = $('<p class="price-review">Price: €' + product.price + ' | Review: ' + product.review + '</p>');
-              var cardButton = $('<button class="btn btn-primary card-button">Add to Cart</button>');
-  
-              cardBody.append(cardTitle);
-              cardBody.append(cardText);
-              cardFooter.append(priceReview);
-              cardFooter.append(cardButton);
-              card.append(cardImage);
-              card.append(cardBody);
-              card.append(cardFooter);
-              cardCol.append(card);
-
-            
-              card.draggable({
-                revert: 'invalid',
-                helper: 'clone',
-                start: function(event, ui) {
-                  ui.helper.css('width', card.width());
-                }
-              });
-
-              cardButton.on('click', function() {
-                var productId = product.id;
-                cart.addToCart(productId);
-              });
-  
-              if (index % 4 === 0) {
-                var row = $('<div class="row"></div>');
-                $('#productList').append(row);
-              }
-  
-              $('#productList .row:last-child').append(cardCol);
-  
-              cardButton.on('click', function() {
-                var productId = product.id;
-                cart.addToCart(productId);
-              });
-            });
-          } else {
-            console.error('Error: No data found.');
-          }
-        },
-        error: function(xhr, status, error) {
-          console.error('AJAX Error: ' + error);
-        }
-      });
     }
-  
+
+    if (searchQuery !== '') {
+      apiUrl += '&search=' + searchQuery;
+    }
+
     fetchProducts(apiUrl);
   });
-  
+
+  $('.search-input').on('input', function () {
+    var selectedCategory = $('.category-dropdown').val();
+    var searchQuery = $(this).val();
+    apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=products';
+
+    if (selectedCategory !== 'none') {
+      apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsByCategory&kategorie=' + selectedCategory;
+    }
+
+    if (searchQuery !== '') {
+      apiUrl = 'http://localhost/SoftwareSupply/backend/api/api.php?type=productsBySearch&search=' + searchQuery;
+    }
+
+    fetchProducts(apiUrl);
+  });
+
+  function fetchProducts(apiUrl) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      contentType: 'application/json',
+      url: apiUrl,
+      success: function (response) {
+        if (response.length > 0) {
+          var productList = $('#productList');
+          productList.empty();
+
+          response.forEach(function (product, index) {
+            var imagePath = 'http://localhost/SoftwareSupply/backend/productpictures/' + product.image_url + '.png';
+            var cardCol = $('<div class="col-md-3 d-flex"></div>');
+            var card = $('<div class="card mb-4"></div>');
+            var cardImage = $('<img class="card-img-top small-icon" src="' + imagePath + '" alt="software">');
+            var cardBody = $('<div class="card-body"></div>');
+            var cardTitle = $('<h5 class="card-title">' + product.title + '</h5>');
+            var cardText = $('<p class="card-text description">' + product.description + '</p>');
+            var cardFooter = $('<div class="card-footer"></div>');
+            var priceReview = $('<p class="price-review">Price: €' + product.price + ' | ' + product.review + '/5 &star;</p>');
+            var cardButton = $('<button class="btn btn-primary card-button"><i class="fas fa-shopping-cart"></i></button>');
+
+            cardBody.append(cardTitle);
+            cardBody.append(cardText);
+            cardFooter.append(priceReview);
+            cardFooter.append(cardButton);
+            card.append(cardImage);
+            card.append(cardBody);
+            card.append(cardFooter);
+            cardCol.append(card);
+
+
+            card.draggable({
+              revert: 'invalid',
+              helper: 'clone',
+              start: function (event, ui) {
+                ui.helper.css('width', card.width());
+              }
+            });
+
+            cardButton.on('click', function () {
+              var productId = product.id;
+              cart.addToCart(productId);
+            });
+
+            var maxDescriptionLength = 100;
+            if (product.description.length > maxDescriptionLength) {
+              var truncatedDescription = product.description.substring(0, maxDescriptionLength) + '...';
+              cardText.text(truncatedDescription);
+            }
+
+            cardBody.addClass('d-flex flex-column justify-content-between');
+
+            if (index % 4 === 0) {
+              var row = $('<div class="row"></div>');
+              $('#productList').append(row);
+            }
+
+            $('#productList .row:last-child').append(cardCol);
+
+            cardButton.on('click', function () {
+              var productId = product.id;
+              cart.addToCart(productId);
+            });
+          });
+        } else {
+          console.error('Error: No data found.');
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error('AJAX Error: ' + error);
+      }
+    });
+  }
+
+  fetchProducts(apiUrl);
+});
